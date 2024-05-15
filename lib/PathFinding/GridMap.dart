1b8621c6823c35accf463cab2b9b867576ce271d
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2024-05-15 15:28:39 -0300

    Adionar mais um construtor para o GridMap
    - Adicionei um construtor em que vc passa apenas o tamanho da matriz ele gera uma matriz com todas suas celulas marcadas como Walkable

--- lib/PathFinding/GridMap.dart
+++ lib/PathFinding/GridMap.dart
@@ -4,14 +4,32 @@ class GridMap {
   late List<List<CellEntity>> _cellMatrix;
 
   GridMap(List<List<int>> matrix) {
-    _cellMatrix = List<List<CellEntity>>.generate(matrix.length, (int i)=>List<CellEntity>.generate(matrix[0].length, (int i)=>new CellEntity(0,0,false), growable: false), growable: false);
-    for(int i = 0; i < matrix.length; i++) {
-      for(int j = 0; j < matrix[i].length; j++) {
-        _cellMatrix[i][j] = new CellEntity(i,j,matrix[i][j] == 1);
+    _cellMatrix = List<List<CellEntity>>.generate(
+        matrix.length,
+        (int i) => List<CellEntity>.generate(
+            matrix[0].length, (int i) => CellEntity(0, 0, false),
+            growable: false),
+        growable: false);
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        _cellMatrix[i][j] = new CellEntity(i, j, matrix[i][j] == 1);
       }
     }
   }
 
+  GridMap.AllWalkable(int numRows, int numColumns) {
+    _cellMatrix = List<List<CellEntity>>.generate(
+        numRows,
+        (int i) => List<CellEntity>.generate(
+            numColumns, (int i) => CellEntity(0, 0, true),
+            growable: false),
+        growable: false);
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numColumns; j++) {
+        _cellMatrix[i][j] = CellEntity(i, j, true);
+      }
+    }
+  }
   List<List<CellEntity>> getCellsMatrix() {
     return _cellMatrix;
   }
