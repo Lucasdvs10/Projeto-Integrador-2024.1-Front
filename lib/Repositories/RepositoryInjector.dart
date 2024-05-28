97ff04c1f8622751659a9e619a45f0359842527d
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2024-05-28 16:16:21 -0300

    Add classe reponsavel por retornar os repositorio
    - A classe tem um booleano que determina se vamos usar os repositorios mockado ou nao

---
+++ lib/Repositories/RepositoryInjector.dart
@@ -0,0 +1,41 @@
+import 'package:projeto_integrador/Repositories/AdivsorRepoMock.dart';
+import 'package:projeto_integrador/Repositories/IAdvisorRepo.dart';
+import 'package:projeto_integrador/Repositories/IProjectRepo.dart';
+import 'package:projeto_integrador/Repositories/IStudentRepo.dart';
+import 'package:projeto_integrador/Repositories/ProjectRepoMock.dart';
+import 'package:projeto_integrador/Repositories/StudentRepoMock.dart';
+
+class RepositoryInjector {
+  static const bool IS_TEST_ENVIROMENT = true;
+
+  static IProjectRepo GetProjectRepo() {
+    if (IS_TEST_ENVIROMENT) {
+      return ProjectRepoMock();
+    } else {
+      // return ProjectRepo(); //Retorna o repositorio real
+      //Todo: Fazer o repositório real
+      return ProjectRepoMock();
+    }
+  }
+
+  static IStudentRepo GetStudentRepo() {
+    if (IS_TEST_ENVIROMENT) {
+      return StudentRepoMock();
+    } else {
+      // return StudentRepo(); //Retorna o repositorio real
+      //Todo: Fazer o repositório real
+      return StudentRepoMock();
+    }
+  }
+
+  static IAdvisorRepo GetAdvisorRepo() {
+    if (IS_TEST_ENVIROMENT) {
+      return AdvisorRepoMock();
+    } else {
+      // return AdvisorRepo(); //Retorna o repositorio real
+      //Todo: Fazer o repositório real
+      return AdvisorRepoMock();
+    }
+  }
+
+}
