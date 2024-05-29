65e0e3d72a7e89e1dfa85528430a02dda0297ef4
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2024-05-29 16:23:37 -0300

    Tela do orientador com repo mock

--- lib/Entities/AdvisorEntity.dart
+++ lib/Entities/AdvisorEntity.dart
@@ -4,6 +4,7 @@ class AdvisorEntity {
   late int id;
   late String name;
   late List<StudentEntity> students;
+  late String projectName;
 
-  AdvisorEntity({required this.id, required this.name, required this.students});
+  AdvisorEntity({required this.id, required this.name, required this.students, required this.projectName});
 }
