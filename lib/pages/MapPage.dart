8cb9793aeccf24e7bf33ec034cf344d3667705eb
Victor Cecche Pregnolatto <vcecche@gmail.com>
2024-06-11 18:23:14 -0300

    Correções estéticas e de funcionalidade

--- lib/pages/MapPage.dart
+++ lib/pages/MapPage.dart
@@ -12,9 +12,9 @@ class MapPage extends StatefulWidget {
 
   MapPage(
       {super.key,
-      this.simulatedTapPosition = Offset.zero,
-      this.startPoint,
-      this.endPoint});
+        this.simulatedTapPosition = Offset.zero,
+        this.startPoint,
+        this.endPoint});
 
   @override
   MapPageState createState() => MapPageState(startPoint, endPoint);
@@ -39,98 +39,42 @@ class MapPageState extends State<MapPage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xFF0A2E93),
-        title: Row(
-          children: [
-            IconButton(
-              onPressed: () async {
-                ((int, int), (int, int))? coordinatesTuple =
-                    await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SearchOptionsPage()),
-                );
+        iconTheme: const IconThemeData(
+          color: Colors.white, // Define a cor da seta de voltar como branca
+        ),
+        leading: IconButton(
+          onPressed: () async {
+            ((int, int), (int, int))? coordinatesTuple =
+            await Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const SearchOptionsPage()),
+            );
 
-                setState(() {
-                  if (coordinatesTuple == null) return;
-                  startPoint = coordinatesTuple.$1;
-                  endPoint = coordinatesTuple.$2;
-                  mapWidget.CalculatePathAndRender(startPoint!, endPoint!);
-                });
-              },
-              icon: const Icon(
-                Icons.search,
+            setState(() {
+              if (coordinatesTuple == null) return;
+              startPoint = coordinatesTuple.$1;
+              endPoint = coordinatesTuple.$2;
+              mapWidget.CalculatePathAndRender(startPoint!, endPoint!);
+            });
+          },
+          icon: const Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+        ),
+        flexibleSpace: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: const Text(
+              'EUREKA',
+              style: TextStyle(
+                fontFamily: 'Dongle',
+                fontSize: 36,
                 color: Colors.white,
               ),
             ),
-            const Spacer(),
-            const Padding(
-              padding: EdgeInsets.only(right: 100.0),
-              child: Text(
-                'EUREKA',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            const Spacer(),
-          ],
-        ),
-      ),
-      endDrawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color(0xFF0A2E93),
-              ),
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              title: const Text('Tela Inicial'),
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SearchOptionsPage()),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('Pesquisa por nome do aluno'),
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const StudentSearchPage()),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('Pesquisa por nome de projeto'),
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ProjectSearchPage()),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('Pesquisa por nome de Orientador'),
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const AdvisorSearchPage()),
-                );
-              },
-            ),
-          ],
+          ),
         ),
       ),
       body: Container(
