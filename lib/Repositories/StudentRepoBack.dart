c903c8394c42ef5c8156948aa4f14e3c9610360b
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2024-06-12 10:17:50 -0300

    Implementar getall students ainda sem testar
    - Implementei a função de getllstudents, mas ainda não testei
    - Escrevi mais testes para o factory de Stundent.FromJson para entender melhor como a função jsonDecode funciona

--- lib/Repositories/StudentRepoBack.dart
+++ lib/Repositories/StudentRepoBack.dart
@@ -1,13 +1,45 @@
 import 'package:projeto_integrador/Entities/StudentEntity.dart';
 import 'package:projeto_integrador/Repositories/IStudentRepo.dart';
 
-class StudentRepoBack implements IStudentRepo{
-  StudentRepoBack(){}
+class StudentRepoBack implements IStudentRepo {
+  Future<Map<String, dynamic>> MakeAGetRequestToAPI() {
+    // Aqui terá a lógica de fazer a requisição pra API
+    // Instanciar um json com a resposta e retorná-lo
+    throw UnimplementedError();
+  }
 
   @override
-  Future<List<StudentEntity>> GetAllStudents() {
-    // TODO: implement GetAllStudents
-    throw UnimplementedError();
+  Future<List<StudentEntity>> GetAllStudents() async {
+    //O formato do json esperado é:
+    /*
+    {
+      [
+        {
+         'idAluno': 1234,
+         'nomeAluno': 'Lucas Duez',
+         "trabalho":{
+           "estande":{
+             "numEstande": 15
+            }
+          }
+        },
+        {
+          outro aluno...
+        }
+      ]
+    }
+    */
+
+    var response = await MakeAGetRequestToAPI();
+    var allStudentsJson = response['allStudents'];
+
+    List<StudentEntity> listToReturn = [];
+
+    for (var student in allStudentsJson) {
+      listToReturn.add(StudentEntity.FromJson(student as Map<String, dynamic>));
+    }
+
+    return Future.value(listToReturn);
   }
 
   @override
